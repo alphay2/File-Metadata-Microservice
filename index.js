a5e500da5f1af08042bd4634618983f53d9340fd
alphay2 <mingfuk.inv@gmail.com>
2024-09-27 15:19:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,22 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.get('/hello', function(req, res) {
+  res.json({greetings: "Hello, API"});
+});
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+let multer = require('multer')
+
+app.post('/api/fileanalyse', multer().single('upfile'), (request, response) => {
+  let responseObject = {}
+  responseObject['name'] = request.file.originalname
+  responseObject['type'] = request.file.mimetype
+  responseObject['size'] = request.file.size
+  response.json(responseObject)
+})
